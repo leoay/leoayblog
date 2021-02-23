dbcc0ee0f26b5f65f5453853ceed3c657f5c9cf6
leoay <leoay23@hotmail.com>
2021-02-23 11:48:37 +0800

    update

--- killall.sh
+++
@@ -1,9 +0,0 @@
-allPids=`pidof chrome`
-# echo $allPids
-arr=`echo $allPids | tr ' ' ' '`
-# echo $arr
-for var in $arr;
-do
-    echo $var
-    kill -9 $var
-done
--- push.sh
+++
@@ -1,1 +0,0 @@
-ffmpeg -f x11grab -s 1920x1080 -i :0.0 -vcodec libx264 -f flv rtsp://127.0.0.1:554/your_strea
